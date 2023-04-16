b47ecdbc53c5328863727f9db4c40230af2d56e0
Your Name <reyadhasan7254@gmail.com>
2023-04-16 15:59:02 +0600

    upto post api added

---
+++ TodoApp/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI , Depends , HTTPException
+from database import engine, SessionLocal
+import models
+from sqlalchemy.orm import Session
+from pydantic import BaseModel,Field
+from typing import Optional
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+       
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0,lt=6,description='priority must be between 1-5')
+    complete: bool
+
+
+@app.get('/')
+async def read_all(db: Session=Depends(get_db)):
+    return db.query(models.Todos).all()
+
+@app.get('/todo/{todo_id}')
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise 
+
+@app.post('/')
+async def create_todo(todo:Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status':201 ,
+        'transaction': 'Successfully'
+    }
+
+
+def http_exception():
+    return HTTPException(status_code=404,detail='Todos Not Found')
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -3,6 +3,7 @@ from database import Base
 
 class Todos(Base):
     __tablename__ = 'todos'
+    
     id = Column(Integer, primary_key=True , index= True)
     title = Column(String)
     description = Column(String)
